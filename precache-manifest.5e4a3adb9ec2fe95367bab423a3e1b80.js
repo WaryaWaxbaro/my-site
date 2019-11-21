1fbd7725ed36be926d4a5b2b34c881ff4be77787
Warya Waxbaro <warya.waxbaro@gmail.com>
2019-11-21 11:24:49 +0200

    Updates

---
+++ precache-manifest.5e4a3adb9ec2fe95367bab423a3e1b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698dea29cc8f49b8cf42a26635ac2a86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee246969f193ff7ae15",
+    "url": "/static/css/main.277bf871.chunk.css"
+  },
+  {
+    "revision": "92e98ce4b15481eab8bd",
+    "url": "/static/js/2.f4ef1bff.chunk.js"
+  },
+  {
+    "revision": "aee246969f193ff7ae15",
+    "url": "/static/js/main.8a06510e.chunk.js"
+  },
+  {
+    "revision": "48017763f0eae4d6a518",
+    "url": "/static/js/runtime-main.4ebfcfe4.js"
+  },
+  {
+    "revision": "4a669a4bad737b4943afcabf23f4453e",
+    "url": "/static/media/profile.4a669a4b.png"
+  }
+]);
